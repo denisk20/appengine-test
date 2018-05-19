4ec2443cd21cfbc8365ad8e72f80aeeea848915a
Denis Kniazhev <dkniazhev@spredfast.com>
2018-05-19 14:56:39 +0300

    initial commit

---
+++ src/main/java/com/microcure/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.microcure;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@SuppressWarnings("serial")
+@WebServlet
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
